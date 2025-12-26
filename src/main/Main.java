f31fe2cb89252f097ac990ddf0bceece7bd94441
WhosG <alaga.hp@gmail.com>
2025-12-26 21:15:09 +0700

    CREDIT

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,20 @@
+// saya Ajipati Alaga Putra dengan NIM 2409682
+// mengerjakan UTS dalam mata kuliah DPBO
+// untuk keberkahannya maka saya tidak akan melakukan kecurangan
+// sepertu yang telah di spesifikasikan Aamiin.
+
+
+// CREDIT GAME ASSET
+// MAP.............(GEMINI AI)
+// CHARACTER.............(https://elvgames.itch.io/free-fantasy-dreamland-sprites)
+// BULLET(PELURU).............(https://craftpix.net/freebies/free-water-and-fire-magic-sprite-vector-pack/)
+// OBSTACLE(RINTANGAN).............(https://anokolisa.itch.io/free-pixel-art-asset-pack-topdown-tileset-rpg-16x16-sprites)
+// MONSTER.............(https://deepdivegamestudio.itch.io/demon-sprite-pack)
+// MUSIC.............(https://pixabay.com/id/music/petualangan-pixel-quest-364092/)
+// SOUND EFECT TEMBAKAN.............(https://leohpaz.itch.io/50-rpg-battle-magic-sfx)
+// POTION.............(GEMINI AI)
+// HEALTH POINT ICON.............(https://elvgames.itch.io/free-inventory-asset-pack)
+
 package main;
 
 import view.GameWindow;
